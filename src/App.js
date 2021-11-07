8838b3b1cf10cc5de6d7244aec994432e581d880
Mehmet Eren Olgun <51908811+erenolgun@users.noreply.github.com>
2021-11-08 01:32:07 +0300

    Include react-autosuggest package and update UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,63 @@
 import "./App.css";
+import Autosuggest from "react-autosuggest";
+import { useState } from "react";
+
+const searchQuery = [
+  {
+    name: "lorem",
+  },
+  {
+    name: "lorem",
+  },
+  {
+    name: "lorem",
+  },
+  {
+    name: "lorem",
+  },
+];
+
+const getSuggestions = (value) => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0
+    ? []
+    : searchQuery.filter(
+        (lang) => lang.name.toLowerCase().slice(0, inputLength) === inputValue
+      );
+};
+
+const getSuggestionValue = (suggestion) => suggestion.name;
+
+const renderSuggestion = (suggestion) => <div>{suggestion.name}</div>;
 
 function App() {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const onChange = (event, { newValue }) => {
+    setValue(newValue);
+  };
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    setSuggestions(getSuggestions(value));
+  };
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const inputProps = {
+    placeholder: "Search free high-resolution photos",
+    value,
+    onChange: onChange,
+  };
+
+  const submitHandler = () => {
+    console.log(value);
+  };
+
   return (
     <div className='mx-10 md:mx-20 lg:mx-auto lg:max-w-4xl xl:max-w-5xl'>
       <nav className='hidden md:flex md:justify-between md:mt-10'>
@@ -20,26 +77,21 @@ function App() {
             Enter a keyword to search for a photo!
           </p>
           <div className='flex mt-4'>
-            <input
-              className='rounded-l-md py-1.5 outline-none px-2 w-full'
-              type='text'
+            <Autosuggest
+              suggestions={suggestions}
+              onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+              onSuggestionsClearRequested={onSuggestionsClearRequested}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              inputProps={inputProps}
             />
             <button
               className='text-sm bg-yellow-100 rounded-r-md px-3'
               type='submit'
+              onClick={submitHandler}
             >
-              <i class='fas fa-search'></i>
+              <i className='fas fa-search'></i>
             </button>
-          </div>
-          <div className='flex flex-col justify-between text-sm mt-6 px-1 md:text-base'>
-            <p className='underline mb-1'>Suggested keywords:</p>
-            <div className='md:flex md:flex-row'>
-              <p className='cursor-pointer md:mr-10 xl:mr-20'>lorem</p>
-              <p className='cursor-pointer md:mr-10 xl:mr-20'>lorem</p>
-              <p className='cursor-pointer md:mr-10 xl:mr-20'>lorem</p>
-              <p className='cursor-pointer md:mr-10 xl:mr-20'>lorem</p>
-              <p className='cursor-pointer md:mr-10 xl:mr-20'>lorem</p>
-            </div>
           </div>
         </div>
       </header>
